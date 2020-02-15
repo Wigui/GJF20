c01328608e13b87ee90c20e5e9f6003c1fa9064e
Arthur <arthurroche4329@gmail.com>
2020-02-15 17:01:46 +0100

    V4 Code

---
+++ GameJamFiliere/Assets/LD/Scripts/SwitchAreaComponent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAreaComponent : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _elementsIn;
+
+    [SerializeField]
+    private GameObject[] _elementsOut;
+
+    private bool _in;
+
+    private bool _canSwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canSwitch = true;
+        _in = false;
+        DoSwitch();
+    }
+
+    private void DoSwitch()
+    {
+        foreach(GameObject element in _elementsIn)
+        {
+            element.SetActive(_in);
+        }
+
+        foreach (GameObject element in _elementsOut)
+        {
+            element.SetActive(!_in);
+        }
+    }
+
+    public void Switch(bool value)
+    {
+        if (!_canSwitch)
+            return;
+
+        _in = value;
+        _canSwitch = false;
+        DoSwitch();
+    }
+
+    public void CanSwitchAgain()
+    {
+        _canSwitch = true;
+    }
+}
---
+++ GameJamFiliere/Assets/LD/Scripts/TriggerComponent.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerComponent : MonoBehaviour
+{
+    /// <summary>
+    /// Tags autorisés à déclencer le trigger
+    /// </summary>
+    [SerializeField]
+    private string _tagFilter;
+
+    /// <summary>
+    /// Evènements à déclencher lors d'un trigger enter
+    /// </summary>
+    [SerializeField]
+    private UnityEvent _onTriggerEnter;
+
+    /// <summary>
+    /// Evènements à déclencher lors d'un trigger stay
+    /// </summary>
+    [SerializeField]
+    private UnityEvent _onTriggerStay;
+
+    /// <summary>
+    /// Evènements à déclencher lors d'un trigger exit
+    /// </summary>
+    [SerializeField]
+    private UnityEvent _onTriggerExit;
+
+    [SerializeField]
+    private bool _destroyActorOnTrigger;
+
+    [SerializeField]
+    private bool _destroyVictimOnTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != _tagFilter)
+            return;
+
+        _onTriggerEnter?.Invoke();
+
+        if (_destroyActorOnTrigger)
+            Destroy(collision.gameObject);
+
+        if (_destroyVictimOnTrigger)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != _tagFilter)
+            return;
+
+        _onTriggerStay?.Invoke();
+
+        // if (_destroyActorOnTrigger)
+        // Destroy(collision.gameObject);
+
+        //if (_destroyVictimOnTrigger)
+        // Destroy(gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != _tagFilter)
+            return;
+
+        _onTriggerExit?.Invoke();
+
+        // if (_destroyActorOnTrigger)
+        // Destroy(collision.gameObject);
+
+        //if (_destroyVictimOnTrigger)
+        // Destroy(gameObject); 
+    }
+}
